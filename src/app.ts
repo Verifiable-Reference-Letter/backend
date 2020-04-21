d0428a48c2cff76ba00aa270d4c410d9b6570446
Maxim Vezenov <mav417@lehigh.edu>
2020-04-21 14:40:02 -0400

    last try for today

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,12 @@ import cors from 'cors';
 import path from 'path';
 const app = express();
 const port = process.env.PORT || 8080; // default port to listen
+console.log("Dynamic port: " + process.env.PORT);
+console.log("Port chosen: " + port);
 // Serve static files from the React app
 import { router as testAPIRouter } from './routes/testApi';
 
 app.use(express.static(path.join(__dirname, '../build')));
-// start the Express server
-app.listen( port, () => {
-    console.log( `server listening on port:${ port }` );
-} );
-
 app.use(cors());
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
@@ -24,5 +21,10 @@ app.get( "/bye", (req, res) => {
 });
 
 app.use("/testAPI", testAPIRouter);
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server listening on port:${ port }` );
+} );
 
 module.exports = app;
