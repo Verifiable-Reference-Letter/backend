1ba944e79a9f479c1761d957bb6bbc989ca87aee
Maxim Vezenov <mav417@lehigh.edu>
2020-04-21 13:18:14 -0400

    initial backend commit

---
+++ build/precache-manifest.2ac186bdf6850c2339826fd30ccfb240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a863f71df4624605a5b2fcf147b267ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3391b8e7d07d38c7865",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f70484644001592f5cf1",
+    "url": "/static/css/main.a4c59214.chunk.css"
+  },
+  {
+    "revision": "e3391b8e7d07d38c7865",
+    "url": "/static/js/2.3ee1fe10.chunk.js"
+  },
+  {
+    "revision": "9956f638ca73b88e6779ed946b7af8bb",
+    "url": "/static/js/2.3ee1fe10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f70484644001592f5cf1",
+    "url": "/static/js/main.ae49e3d9.chunk.js"
+  },
+  {
+    "revision": "82df78fc457599a0ae7b",
+    "url": "/static/js/runtime-main.3f5e60e9.js"
+  }
+]);
