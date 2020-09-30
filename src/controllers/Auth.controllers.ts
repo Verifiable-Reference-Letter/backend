7d71fa4e1bc553dfdb11d89fe39164b9619e17d9
BZchu <brianzhao24@gmail.com>
2020-09-30 16:58:48 -0400

    fix auth route; discover custom headers not allowed with cors

--- src/controllers/Auth.controllers.ts
+++ src/controllers/Auth.controllers.ts
@@ -6,23 +6,29 @@ const router = express.Router();
 
 const authModule: AuthModule = new AuthModule();
 
+
 // TODO: change this to be hidden
 const jwtKey: string = "my private key";
 
 router.post('/', async (req, res, next) => {
-    console.log("Getting all users");
+    console.log("Authenticating");
     // TODO: check with frontend what is being passed 
     const publicAddress: string = req.body.publicAddress;
     const authResult = await authModule.authorizeUser(req.body.signature, publicAddress);
 
     let jwtToken;
-    if (authResult == true) {
+    if (authResult === false /*true*/) { // TODO: currently verification is broken
+        console.log("verified success");
         jwtToken = jwt.sign({ publicAddress }, jwtKey, {
             algorithm: "HS256",
             expiresIn: "1h",
         })
+        console.log(jwtToken);
+        res.send(JSON.stringify({
+            auth: { jwtToken: jwtToken, publicAddress: publicAddress},
+            data: []
+        }));
     }
-    res.send(jwtToken);
 });
 
 export { router }
