9eafc25b09038d22fde0c92a316a62b317959be0
BZchu <brianzhao24@gmail.com>
2020-10-11 22:16:45 -0400

    fix letter controller

--- src/controllers/Letters.controllers.ts
+++ src/controllers/Letters.controllers.ts
@@ -7,24 +7,38 @@ const router = express.Router();
 const letterModule: LetterModule = new LetterModule();
 
 // TODO: change these to get the user id from a verified JWT token once we implement logging in functionality
-router.post('/received', async (req, res, next) => {
-    const letterModels: Letter[] = await letterModule.selectAllLettersByRequestorAddress(req.params.publicAddress);
-    // res.json(letterModels);
+
+router.post('/requested', async (req, res, next) => {
+    // TODO: check JWT
+    console.log(req.body['auth']);
+    console.log("get letter for requestor");
+    const letterModels: Letter[] = await letterModule.selectAllLettersByRequestorAddress(req.body["auth"].publicAddress);
+    console.log(letterModels);
+    res.json(letterModels);
 });
 
 router.post('/written', async (req, res, next) => {
-    const letterModels: Letter[] = await letterModule.selectAllLettersByWriterAddress(req.params.publicAddress);
-    // res.json(letterModels);
+    // TODO: check JWT
+    console.log(req.body['auth']);
+    console.log("get letter for writer");
+    const letterModels: Letter[] = await letterModule.selectAllLettersByWriterAddress(req.body["auth"].publicAddress);
+    console.log(letterModels);
+    res.json(letterModels);
 });
 
-router.post('/requested', async (req, res, next) => {
-    const letterModels: LetterHistory[] = await letterModule.selectAllLettersByRecipientAddress(req.params.publicAddress);
-    // res.json(letterModels);
+router.post('/received', async (req, res, next) => {
+    // TODO: check JWT
+    console.log(req.body['auth']);
+    console.log("get letter history for recipient");
+    const letterHistoryModels: LetterHistory[] = await letterModule.selectAllLettersByRecipientAddress(req.body["auth"].publicAddress);
+    console.log(letterHistoryModels);
+    res.json(letterHistoryModels);
 });
 
 router.post('/:letterId/history', async (req, res, next) => {
+    // TODO: check JWT
     console.log(req.body['auth']);
-    console.log("get letter history");
+    console.log("get letter history for given letter_id");
     const letterHistoryModels: LetterHistory[] = await letterModule.selectAllLetterHistoryByLetterId(req.params.letterId);
     console.log(letterHistoryModels);
     res.json(letterHistoryModels);
