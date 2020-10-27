c494c8e568442bd41e52eedfff454dd86597f778
stevenshi99 <34109240+stevenshi99@users.noreply.github.com>
2020-10-27 11:43:58 -0400

    Update Emails.module.ts
    removed explicit mention of api key

--- src/modules/Emails.module.ts
+++ src/modules/Emails.module.ts
@@ -3,16 +3,11 @@ import SendGrid from "@sendgrid/mail";
 export class EmailsModule {
 
     constructor() {
-        SendGrid.setApiKey("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"
+        SendGrid.setApiKey(process.env.SENDGRID_API_KEY
             );
     }
-//"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"
-    public sendMail() {
 
-        // using Twilio SendGrid's v3 Node.js Library
-        // https://github.com/sendgrid/sendgrid-nodejs
-        // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        //this.sgMail.setApiKey('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');
+    public sendMail() {
         const msg = {
         to: 'stevenshi1999@gmail.com',
         from: 'teamgas2020@gmail.com',
