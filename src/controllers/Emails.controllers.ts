a9203e4e06801e0ee6ee434cca8ec0949e7811e8
Maxim Vezenov <mav417@lehigh.edu>
2020-11-01 15:26:04 -0500

    tiny change, commented out email controller

--- src/controllers/Emails.controllers.ts
+++ src/controllers/Emails.controllers.ts
@@ -1,15 +1,15 @@
-import express from 'Express';
-import { EmailsModule } from '../modules/Emails.module';
+// import express from 'Express';
+// import { EmailsModule } from '../modules/Emails.module';
 
-const router = express.Router();
+// const router = express.Router();
 
-const emailsModule: EmailsModule = new EmailsModule();
+// const emailsModule: EmailsModule = new EmailsModule();
 
-router.get('/sendEmail', async (req, res, next) => {
-    console.log('sending email');
-    // console.log({ key: process.env.SENDGRID_API_KEY });
-    await emailsModule.sendMail();
-    res.send("hello");
-});
+// router.get('/sendEmail', async (req, res, next) => {
+//     console.log('sending email');
+//     // console.log({ key: process.env.SENDGRID_API_KEY });
+//     await emailsModule.sendMail();
+//     res.send("hello");
+// });
 
-export { router };
+// export { router };
