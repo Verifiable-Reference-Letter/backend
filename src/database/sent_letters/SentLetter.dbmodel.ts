6e3646218de34ec5c0ceeadc98c9663c2e54be5e
BZchu <brianzhao24@gmail.com>
2020-11-03 12:05:48 -0500

    fix sentletter queries and sentRecipients;
    add sentRecipients query to userdbservice; update letters controller

--- src/database/sent_letters/SentLetter.dbmodel.ts
+++ src/database/sent_letters/SentLetter.dbmodel.ts
@@ -1,27 +1,37 @@
 export class SentLetter {
+  letterRecipient: string;
+  letterId: string;
+  sentAt: Date | null;
+  letterContents: string;
+  letterSignature: string;
 
-    letterRecipient: string;
-    letterId: string;
-    sentAt: Date | null;
-
-    constructor(
-        letterRecipient: string,
-        letterId: string,
-        sentAt: Date | null,
-    ) {
-        this.letterRecipient = letterRecipient;
-        this.letterId = letterId;
-        this.sentAt = sentAt;
+  constructor(
+    letterRecipient: string,
+    letterId: string,
+    sentAt: Date | null,
+    letterContents: Buffer,
+    letterSignature: string
+  ) {
+    this.letterRecipient = letterRecipient;
+    this.letterId = letterId;
+    this.sentAt = sentAt;
+    if (letterContents !== null) {
+      this.letterContents = letterContents.toString("utf8");
+    } else {
+      this.letterContents = null;
     }
+    this.letterSignature = letterSignature;
+  }
 
-    static dbRowToDbModel(dbRow: any): SentLetter {
-        const newSentLetter = new SentLetter(
-            dbRow.letter_recipient,
-            dbRow.letter_id,
-            dbRow.sent_at,
-        );
-        console.dir(newSentLetter);
-        return newSentLetter;
-    }
-
+  static dbRowToDbModel(dbRow: any): SentLetter {
+    const newSentLetter = new SentLetter(
+      dbRow.letter_recipient,
+      dbRow.letter_id,
+      dbRow.sent_at,
+      dbRow.letter_contents,
+      dbRow.letter_signature
+    );
+    console.dir(newSentLetter);
+    return newSentLetter;
+  }
 }
--- src/database/sent_letters/SentLetter.dbservice.ts
+++ src/database/sent_letters/SentLetter.dbservice.ts
@@ -23,8 +23,8 @@ export class SentLetterDbService extends DatabaseService<SentLetter> {
      * select all sent letter rows by recipient's address
      * @param id
      */
-    async selectAllSentLettersByRecipientAddress(id: string): Promise<SentLetter[]> {
-        const values = [id];
+    async selectAllSentLettersByRecipientAddress(publicAddress: string): Promise<SentLetter[]> {
+        const values = [publicAddress];
         return super.runParameterizedQueryWithValuesArray(this.selectAllSentLettersByRecipientQuery, values);
     }
 
@@ -32,21 +32,34 @@ export class SentLetterDbService extends DatabaseService<SentLetter> {
      * select all sent letter rows by letter id
      * @param id 
      */
-    async selectAllSentLettersByLetterId(id: string): Promise<SentLetter[]> {
-        const values = [id];
+    async selectAllSentLettersByLetterId(letterId: string): Promise<SentLetter[]> {
+        const values = [letterId];
         return super.runParameterizedQueryWithValuesArray(this.selectAllSentLettersByLetterIdQuery, values);
     }
 
+    
+    /**
+     * select all sent letter rows by letter id
+     * @param id 
+     */
+    async selectAllSentLettersByLetterIdAndRecipient(letterId: string, publicAddress: string): Promise<SentLetter[]> {
+        const values = [letterId, publicAddress];
+        return super.runParameterizedQueryWithValuesArray(this.selectAllSentLettersByLetterIdAndRecipientQuery, values);
+    }
+
+    private selectAllSentLettersByRecipientQuery = {
+        text: 'SELECT * from ' + sentLetterTableName + ' WHERE letter_recipient = $1;'
+    }
+
     private selectAllSentLettersByLetterIdQuery = {
-        text: 'SELECT * from ' + sentLetterTableName + ' WHERE letter_id = $1'
+        text: 'SELECT * from ' + sentLetterTableName + ' WHERE letter_id = $1;'
+    }
+
+    private selectAllSentLettersByLetterIdAndRecipientQuery = {
+        text: 'SELECT * from ' + sentLetterTableName + ' WHERE letter_id = $1 and letter_recipient = $2;'
     }
 
     protected dbRowToDbModel(dbRow: any): SentLetter {
         return SentLetter.dbRowToDbModel(dbRow);
     }
-
-    private selectAllSentLettersByRecipientQuery = {
-        text: 'SELECT * from ' + sentLetterTableName + ' WHERE public_address = $1'
-    }
-
 }
